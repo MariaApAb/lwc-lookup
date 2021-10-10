b98c6fe9f6119f0a3a39821dc4ab4ad1a9df99e0
María <maria_aparicio_abad@outlook.com>
2021-10-10 14:40:18 +0200

    DatatableController

--- force-app/main/default/lwc/datatable/datatable.js
+++ force-app/main/default/lwc/datatable/datatable.js
@@ -1,15 +1,6 @@
 import { LightningElement, api, track, wire } from "lwc";
-import getDatatableColumns from "@salesforce/apex/LookupController.getDatatableColumns";
+import getDatatableColumns from "@salesforce/apex/DatatableController.getDatatableColumns";
 
-/*
-const columns = [
-    { label: 'Label', fieldName: 'name' },
-    { label: 'Website', fieldName: 'website', type: 'url' },
-    { label: 'Phone', fieldName: 'phone', type: 'phone' },
-    { label: 'Balance', fieldName: 'amount', type: 'currency' },
-    { label: 'CloseAt', fieldName: 'closeAt', type: 'date' },
-];
-*/
 export default class Datatable extends LightningElement {
     @api fields;
     @api sobjectApiName;
@@ -22,12 +13,12 @@ export default class Datatable extends LightningElement {
         { label: "Phone", fieldName: "Phone", type: "phone" }
     ];
 
-    /*@wire(getDatatableColumns, { sobjectApiName: "$sobjectApiName", fields: "$fields" })
+    @wire(getDatatableColumns, { sobjectApiName: "$sobjectApiName", fields: "$fields" })
     wiredColumns({ error, data }) {
         if (data) {
             this.columns = data;
         } else if (error) {
             console.error(error);
         }
-    }*/
+    }
 }
